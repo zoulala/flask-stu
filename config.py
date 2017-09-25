39e60ca0890da05fa51567ecea4d48e598a745cb
zoulala <zlw2008ok@126.com>
2017-09-25 18:06:06 +0800

    created the base application

---
+++ config.py
@@ -0,0 +1,16 @@
+
+class Config(object):
+    """Base config class."""
+    pass
+
+class ProdConfig(Config):
+    """Production config class."""
+    pass
+
+class DevConfig(Config):
+    """Development config class."""
+    # Open the DEBUG
+    DEBUG = True
+
+    # MySQL connection
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:560320@localhost:3306/flask-stu'
